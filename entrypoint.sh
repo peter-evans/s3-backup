bb8f61e4bb1d2332569b140655ab2208e8fa661d
Andrei Nakagawa <shinitiandrei@hotmail.com>
2022-05-06 14:29:32 +1200

    update: removing duplicate out of conditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,9 @@ REGION=${REGION:=""}
 # Set mc configuration
 if [ -z "$SESSION_TOKEN" ]; then
   mc alias set "$STORAGE_SERVICE_ALIAS" "$STORAGE_SERVICE_URL" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
-  # Execute mc mirror
-  mc mirror $* "$MIRROR_SOURCE" "$STORAGE_SERVICE_ALIAS/$MIRROR_TARGET"
 else
   export MC_HOST_${STORAGE_SERVICE_ALIAS}=https://${ACCESS_KEY_ID}:${SECRET_ACCESS_KEY}:${SESSION_TOKEN}@s3.${REGION}.amazonaws.com
-  # Execute mc mirror
-  mc mirror $* "$MIRROR_SOURCE" "$STORAGE_SERVICE_ALIAS/$MIRROR_TARGET"
 fi
 
+  # Execute mc mirror
+  mc mirror $* "$MIRROR_SOURCE" "$STORAGE_SERVICE_ALIAS/$MIRROR_TARGET"
